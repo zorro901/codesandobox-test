9c6a24e0951a71c2665499af13e46708f8949d00
zorro901 <zoro907@gmail.com>
2021-08-07 06:29:04 +0900

    36. タスクの戻す機能

--- src/index.js
+++ src/index.js
@@ -4,7 +4,16 @@ const onClickAdd = () => {
   // テキストボックスの値を取得して、初期化
   const inputText = document.getElementById("add-text").value; //テキストボックスの値を取得
   document.getElementById("add-text").value = ""; //値を初期化
+  createIncompleteList(inputText);
+};
 
+// 未完了リストから指定の要素を削除
+const deleteFromIncompleteList = (target) => {
+  document.getElementById("incomplete-list").removeChild(target);
+};
+
+// 未完了のリストに追加する関数
+const createIncompleteList = (inputText) => {
   // li生成
   const li = document.createElement("li");
   li.className = "list-row";
@@ -43,9 +52,6 @@ const onClickAdd = () => {
     // div以下を初期化
     addTarget.textContent = null;
 
-    // liタグを生成
-    const li = document.createElement("li");
-
     // p生成
     const p = document.createElement("p");
     p.innerText = text;
@@ -53,15 +59,24 @@ const onClickAdd = () => {
     // button（戻す）生成
     const backButton = document.createElement("button");
     backButton.innerText = "戻す";
-
-    // liタグに要素に各要素を設定
-    addTarget.appendChild(li);
+    backButton.addEventListener("click", () => {
+      // 押された戻すボタンの親タグ（div）を完了リストから削除
+      const deleteTarget = backButton.parentElement.parentNode;
+      document.getElementById("complete-list").removeChild(deleteTarget);
+      backButton.textContent = null; //戻すボタンの文字列をnullへ
+      const text = deleteTarget.firstElementChild.parentElement.innerText;
+      console.log(text);
+      createIncompleteList(text);
+    });
 
     // div生成
     const div = document.createElement("div");
-    li.appendChild(div);
+    addTarget.appendChild(div);
     div.appendChild(p);
     div.appendChild(backButton);
+
+    // liタグに要素に各要素を設定
+    addTarget.appendChild(div);
 
     // 完了リストに追加
     document.getElementById("complete-list").appendChild(addTarget);
@@ -74,11 +89,6 @@ const onClickAdd = () => {
   div.appendChild(p);
   div.appendChild(completeButton);
   div.appendChild(deleteButton);
-};
-
-// 未完了リストから指定の要素を削除
-const deleteFromIncompleteList = (target) => {
-  document.getElementById("incomplete-list").removeChild(target);
 };
 
 document
