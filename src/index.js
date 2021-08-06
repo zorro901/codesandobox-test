94f0152f4186cedcbafe4cd0e796304127bb16fb
zorro901 <zoro907@gmail.com>
2021-08-07 04:05:41 +0900

    33. タスクの追加機能(ボタン)

--- src/index.js
+++ src/index.js
@@ -14,16 +14,22 @@ const onClickAdd = () => {
   const div = document.createElement("div");
   li.appendChild(div);
 
-  // button生成
-  const button_complete = document.createElement("button");
-  div.appendChild(button_complete);
-  button_complete.innerText = "完了";
-  // const button_remove = document.createElement("button");
-  // div.appendChild(button_remove);
-  // div.innerText = "削除";
+  // button（完了）生成
+  const completeButton = document.createElement("button");
+  completeButton.innerText = "完了";
+  completeButton.addEventListener("click", () => alert("完了"));
+
+  // button（削除）生成
+  const deleteButton = document.createElement("button");
+  deleteButton.innerText = "削除";
+  deleteButton.addEventListener("click", () => alert("削除"));
 
   // 未完了のリストに追加
   document.getElementById("incomplete-list").appendChild(li);
+
+  // divタグの子要素に各要素を設定
+  div.appendChild(completeButton);
+  div.appendChild(deleteButton);
 
   console.log(li);
 };
